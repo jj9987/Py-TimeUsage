66f1c960cf777111ea58abb99d23c5707e15dcd2
jj9987 <janar.juusu@gmail.com>
2015-12-14 23:01:06 +0200

    backend updated

--- backend.py
+++ backend.py
@@ -3,14 +3,15 @@ import csv
 import os
 from time import sleep
 
-processes={}
-allok=False
+processes=[]
+all_ok=True
 
 # opens the file for saving data, creates if can not open
-with open("applications.txt") as f:
-	for line in f:
-		line=line.split()
-		processes[line[0]] = int(line[1])
+if(os.path.isfile("applications.txt")):
+	with open("applications.txt") as f:
+		for line in f:
+			line=line.split()
+			processes.append([line[0],int(line[1])])
 print(processes)
 
 def AddNewApplication(processname):
@@ -22,16 +23,36 @@ def GetProcessTime(processname):
 	return processes[processname]
 
 def GetProcessStatus(processname):
-	return "TEST-Running"
-
-p_tasklist = subprocess.Popen('tasklist.exe /fo csv',
+	p_tasklist = subprocess.Popen('tasklist.exe /fo csv',
                               stdout=subprocess.PIPE,
                               universal_newlines=True)
 
-while(allok == True):
 	for p in csv.DictReader(p_tasklist.stdout):
-		if p['Image Name'] in processes:
-			processes[p['Image Name']] = processes[p['Image Name']]+1
+		if(p['Image Name'] == processname):	return "Running"
+
+	return "Not running"
+
+
+i=0
+
+while(all_ok == True):
+	i+=1
+	p_tasklist = subprocess.Popen('tasklist.exe /fo csv',
+                              stdout=subprocess.PIPE,
+                              universal_newlines=True)
+
+	for p in csv.DictReader(p_tasklist.stdout):
+		if(p['Image Name'] == "chrome.exe"):
+			continue
+		for item in processes:
+			if(item[0] == p['Image Name']):
+				item[1] += 1
+				continue
+
+	if(i==300): # updates file after every 5 minutes
+		with open("applications.txt", "w") as f:
+			for item in processes:
+				f.write(item[0]+" "+item[1])
 
 	print(processes)
 	sleep(1)
